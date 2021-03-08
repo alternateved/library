8c9797a471e70870be2203b4d959454b4f70fcda
alternateved <alternateved@gmail.com>
2021-03-08 13:35:15 +0100

    Add popupFunctionality function

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,23 @@
 const popupButton = document.querySelector("#plus-button");
+const closeButton = document.querySelector(".close");
 const popup = document.querySelector("#popup");
 
-function showPopupForm () {
+function showPopupForm() {
   popup.style.display = "block";
 }
 function closePopupForm() {
   popup.style.display = "none";
 }
 
-popupButton.addEventListener("click", showPopupForm);
-
-// When the user clicks anywhere outside of the modal, close it
-window.addEventListener("click", event => {
-  if (event.target == popup) {
-    popup.style.display = "none";
+function popupFunctionality() {
+  popupButton.addEventListener("click", showPopupForm);
+  closeButton.addEventListener("click", closePopupForm);
+  // When the user clicks anywhere outside of the modal, close it
+  window.addEventListener("click", (event) => {
+    if (event.target == popup) {
+      popup.style.display = "none";
+    }
+  });
 }
-});
+
+popupFunctionality();
